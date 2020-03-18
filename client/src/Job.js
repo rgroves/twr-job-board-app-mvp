9883978000ca070d7cb5f23823f0c37cac1fc2a9
Robert Groves <rwgdev@gmail.com>
2020-03-18 18:29:26 -0500

    Create Job component with sample text and add it to Jobs.

---
+++ client/src/Job.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Job() {
+  return (
+    <div className={'job'}>
+      No job yet.
+    </div>
+  );
+}
--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
+import Job from './Job';
+
 export default function Jobs() {
   return (
     <div className="jobs">
       <Typography variant="h1">
         Entry Level Software Jobs
       </Typography>
+      <Job />
     </div>
   );
 }
