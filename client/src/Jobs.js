2955f7b57af758d67dd65fa8ce8a8a0a58118073
Robert Groves <rwgdev@gmail.com>
2020-03-18 18:12:12 -0500

    Create Jobs component and add it to App.

---
+++ client/src/Jobs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function Jobs() {
+  return (
+    <div>
+      <Typography variant="h1">
+        Entry Level Software Jobs
+      </Typography>
+    </div>
+  );
+}
