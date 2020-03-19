315617033ac80dbbf36dbde1e9cce85bb3e31a97
Robert Groves <rwgdev@gmail.com>
2020-03-18 20:18:53 -0500

    Add span tag around title and company name in job list.

--- client/src/Job.js
+++ client/src/Job.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Job({job}) {
   return (
     <div className={'job'}>
-      {job.title}
-      {job.company}
+      <span>{job.title}</span>
+      <span>{job.company}</span>
     </div>
   );
 }
