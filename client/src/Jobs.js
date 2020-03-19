ed04fdf5b1e6d8631876b143825f720e48535300
Robert Groves <rwgdev@gmail.com>
2020-03-19 15:06:09 -0500

    Adjust styling of page title heading.

--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -6,7 +6,7 @@ import Job from './Job';
 export default function Jobs({jobs}) {
   return (
     <div className="jobs">
-      <Typography variant="h1">
+      <Typography variant="h4" component="h1">
         Entry Level Software Jobs
       </Typography>
       {
