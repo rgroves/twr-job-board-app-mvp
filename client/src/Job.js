ba3e53dcc1ce5f06071054c107dc07edb4a0d357
Robert Groves <rwgdev@gmail.com>
2020-03-19 15:29:33 -0500

    Update job component's styling.

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
 
 export default function Job({job}) {
   return (
-    <div className={'job'}>
-      <span>{job.title}</span>
-      <span>{job.company}</span>
-    </div>
+    <Paper className={'job'}>
+      <Typography>{job.title}</Typography>
+      <Typography>{job.company}</Typography>
+    </Paper>
   );
 }
